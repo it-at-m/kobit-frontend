99900f2f2b1031f2edec73b9db0890981f286118
clemens-code <clemens.schuetze@outlook.de>
2023-02-08 13:15:00 +0100

    fix possible undefined

--- frontend/src/features/the-unterstuetzungsfinder/features/the-anlaufstellen/base-fields-contact.vue
+++ frontend/src/features/the-unterstuetzungsfinder/features/the-anlaufstellen/base-fields-contact.vue
@@ -6,24 +6,24 @@
     <v-card-text>
       <v-list v-if="anlaufstelle?.contact">
         <v-list-item
-          v-for="(contact, contactIndex) in anlaufstelle?.contact"
-          :id="`id_base_fields_contact_email${contactIndex}`"
-          :key="contactIndex"
-          :href="`mailto:${contact.email}`"
+            v-for="(contact, contactIndex) in anlaufstelle?.contact"
+            :id="`id_base_fields_contact_email${contactIndex}`"
+            :key="contactIndex"
+            :href="`mailto:${contact.email}`"
         >
           {{ contact.email }}
         </v-list-item>
       </v-list>
       <v-list v-if="hasLinksNotDownloads">
         <div
-          v-for="(link, linkIndex) in anlaufstelle?.links"
-          :key="linkIndex"
+            v-for="(link, linkIndex) in anlaufstelle?.links"
+            :key="linkIndex"
         >
-          <v-list-item     
-            v-if="link.inDownloads === false"
-            :id="`id_base_fields_contact_link_${linkIndex}`"
-            :href="link.url"
-            target="_blank"
+          <v-list-item
+              v-if="link.inDownloads === false"
+              :id="`id_base_fields_contact_link_${linkIndex}`"
+              :href="link.url"
+              target="_blank"
           >
             {{ link.name }}
           </v-list-item>
@@ -42,9 +42,11 @@ export default class BaseFieldsContact extends Vue {
 
   @Prop()
   anlaufstelle?: Anlaufstelle;
-  
+
   get hasLinksNotDownloads(): boolean {
-     return (this.anlaufstelle.links.filter(function(o) { return o.inDownloads == false; }).length > 0 );
+    return (this.anlaufstelle?.links.filter(function (o) {
+      return o.inDownloads == false;
+    }).length > 0);
   }
 }
 </script>
