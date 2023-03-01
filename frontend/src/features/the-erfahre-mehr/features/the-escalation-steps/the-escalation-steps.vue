ae0d001b4eaffca1f34e92feafaddff10194293b
clemens-code <clemens.schuetze@outlook.de>
2023-03-01 15:09:20 +0100

    fix imports

--- frontend/src/features/the-erfahre-mehr/features/the-escalation-steps/the-escalation-steps.vue
+++ frontend/src/features/the-erfahre-mehr/features/the-escalation-steps/the-escalation-steps.vue
@@ -3,33 +3,33 @@
     <BackButton :callback="back"/>
 
     <BasePageContent
-      :icon="icon"
-      :name="name"
-      :info-text="infoText"
-      :is-loading="false"
+        :icon="icon"
+        :name="name"
+        :info-text="infoText"
+        :is-loading="false"
     >
       <div>
         <v-container
-          v-for="(layer, layerIndex) in escalationSteps"
-          :key="layerIndex"
-          class="mb-0  mt-0"
-          fluid
-          :style="{ 'background': layer.color }"
+            v-for="(layer, layerIndex) in escalationSteps"
+            :key="layerIndex"
+            class="mb-0  mt-0"
+            fluid
+            :style="{ 'background': layer.color }"
         >
           <v-row no-gutters>
             <v-col
-              cols="12"
-              sm="12"
-              class="mr-0 pr-0"
+                cols="12"
+                sm="12"
+                class="mr-0 pr-0"
             >
               <v-expansion-panels
-                style="background:none !important;"
-                flat
+                  style="background:none !important;"
+                  flat
               >
                 <v-expansion-panel style="background:none !important;">
                   <v-expansion-panel-header
-                    :key="layerIndex"
-                    class="pl-0"
+                      :key="layerIndex"
+                      class="pl-0"
                   >
                     {{ layer.name }}
                   </v-expansion-panel-header>
@@ -41,37 +41,37 @@
               </v-expansion-panels>
             </v-col>
             <v-col
-              v-for="(step, stepIndex) in layer.steps"
-              :key="stepIndex"
-              class="mt-3"
-              :offset="0"
-              :offset-sm="0"
-              :offset-md="layerIndex"
-              :offset-lg="layerIndex"
-              :offset-xl="layerIndex"
-              cols="12"
-              sm="12"
+                v-for="(step, stepIndex) in layer.steps"
+                :key="stepIndex"
+                class="mt-3"
+                :offset="0"
+                :offset-sm="0"
+                :offset-md="layerIndex"
+                :offset-lg="layerIndex"
+                :offset-xl="layerIndex"
+                cols="12"
+                sm="12"
             >
               <v-row no-gutters>
                 <v-col
-                  :offset="0"
-                  :offset-sm="0"
-                  :offset-md="stepIndex + layerIndex * 2"
-                  :offset-lg="stepIndex + layerIndex * 2"
-                  :offset-xl="stepIndex + layerIndex * 2"
-                  cols="12"
-                  sm="12"
-                  md="12"
-                  lg="12"
-                  xl="12"
+                    :offset="0"
+                    :offset-sm="0"
+                    :offset-md="stepIndex + layerIndex * 2"
+                    :offset-lg="stepIndex + layerIndex * 2"
+                    :offset-xl="stepIndex + layerIndex * 2"
+                    cols="12"
+                    sm="12"
+                    md="12"
+                    lg="12"
+                    xl="12"
                 >
                   <v-expansion-panels
-                    v-model="$data['panel_' + stepIndex]"
-                    :class="{ 'isMobile': $vuetify.breakpoint.smAndDown, 'isDesktop': $vuetify.breakpoint.mdAndUp }"
+                      v-model="$data['panel_' + stepIndex]"
+                      :class="{ 'isMobile': $vuetify.breakpoint.smAndDown, 'isDesktop': $vuetify.breakpoint.mdAndUp }"
                   >
                     <v-expansion-panel>
                       <v-expansion-panel-header
-                        :class="{ 'justify-center text-center': $vuetify.breakpoint.xs || $vuetify.breakpoint.sm }"
+                          :class="{ 'justify-center text-center': $vuetify.breakpoint.xs || $vuetify.breakpoint.sm }"
                       >
                         Stufe {{ stepIndex + (layerIndex * escalationSteps.length) + 1 }}: {{ step.name }}
                       </v-expansion-panel-header>
@@ -92,7 +92,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import {Component, Vue} from "vue-property-decorator";
 import {
   getEscalationSteps
 } from "@/features/the-erfahre-mehr/features/the-escalation-steps/the-escalation-steps-store.module";
@@ -102,7 +102,6 @@ import {
   escalationStepsRoutes
 } from "@/features/the-erfahre-mehr/features/the-escalation-steps/the-escalation-steps.routes";
 import BackButton from "@/features/commons/components/BackButton.vue";
-import {useRouter} from "vue-router/composables";
 
 @Component({
   components: {
