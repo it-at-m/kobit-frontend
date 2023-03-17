7a714908a1cc698ed28c6d02d9bb813d55e8260a
clemens-code <clemens.schuetze@outlook.de>
2023-03-17 15:17:18 +0100

    change admin panel icon

--- frontend/src/features/admin/adminRoutes.ts
+++ frontend/src/features/admin/adminRoutes.ts
@@ -2,7 +2,7 @@ import AdminOverview from "@/features/admin/AdminOverview.vue";
 
 export const ADMIN_PATH = "/admin";
 export const ADMIN_ROUTE_NAME = "Admin";
-export const ADMIN_ICON = "mdi-school";
+export const ADMIN_ICON = "mdi-wrench";
 export const ADMIN_INFO_TEXT = "Hier können Sie änderungen an Inhalten vornehmen.";
 
 
