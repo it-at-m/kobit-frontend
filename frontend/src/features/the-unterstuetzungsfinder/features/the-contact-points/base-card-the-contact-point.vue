107042cd347305cfc646d0a810371e28f36fd540
Thomas Fink <thomas.fink@muenchen.de>
2023-05-16 11:46:55 +0200

    nicer image display

--- frontend/src/features/the-unterstuetzungsfinder/features/the-contact-points/base-card-the-contact-point.vue
+++ frontend/src/features/the-unterstuetzungsfinder/features/the-contact-points/base-card-the-contact-point.vue
@@ -3,52 +3,48 @@
     <LoadingSpinner :is-loading="isLoading" />
     <v-row>
       <v-col>
-        <v-card
-          flat
-          :style="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm? 'border-top:1px solid #eee;' : ''"
-        >
+        <v-card flat :style="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm ? 'border-top:1px solid #eee;' : ''">
           <v-card-title>{{ contactPoint?.name }}</v-card-title>
           <v-card-text>
-            <span v-html="computeMarkdown" />
-            <base-fields-contact
-              v-if="contactPoint?.contact"
-              :contact-point="contactPoint"
-            />
-            <base-fields-additional-content
-              v-if="linksInDownLoads?.length > 0"
-              :links="linksInDownLoads"
-            />
-            <img
-              v-if="contactPoint?.image"
-              :src="contactPoint.image.toString()"
-              style="max-width: 400px;max-height:400px;"
-            >
+            <v-row>
+              <v-col cols="12">
+                <span v-html="computeMarkdown" />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="12" md="12" lg="6" xl="6">
+                <base-fields-contact v-if="contactPoint?.contact" :contact-point="contactPoint" />
+                <base-fields-additional-content v-if="linksInDownLoads?.length > 0" :links="linksInDownLoads" />
+              </v-col>
+              <v-col cols="12" sm="12" md="12" lg="6" xl="6">
+                <img v-if="contactPoint?.image" :src="contactPoint.image.toString()"
+                  style="width: 100%; max-height:400px; object-fit: cover;">
+              </v-col>
+            </v-row>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
 <script lang="ts">
-
-import {computed, defineComponent, ref, watch} from "vue";
-import {useGetContactPoint} from "@/features/commons/middleware/useGetContactPoints";
+import { computed, defineComponent, ref, watch } from "vue";
+import { useGetContactPoint } from "@/features/commons/middleware/useGetContactPoints";
 import LoadingSpinner from "@/features/commons/components/LoadingSpinner.vue";
-import {marked} from "marked";
+import { marked } from "marked";
 import BaseFieldsContact from "@/features/the-unterstuetzungsfinder/features/the-contact-points/base-fields-contact.vue";
 import BaseFieldsAdditionalContent
   from "@/features/the-unterstuetzungsfinder/features/the-contact-points/base-fields-additional-content.vue";
-import {useRoute} from "vue-router/composables";
-import {Link} from "@/features/commons/types/ContactPoint";
+import { useRoute } from "vue-router/composables";
+import { Link } from "@/features/commons/types/ContactPoint";
 
 export default defineComponent({
   name: "BaseCardAnlaufstelle",
-  components: {BaseFieldsAdditionalContent, BaseFieldsContact, LoadingSpinner},
+  components: { BaseFieldsAdditionalContent, BaseFieldsContact, LoadingSpinner },
   setup() {
     const route = useRoute();
     const pageId = ref<string>(route.params.id);
-    const {isLoading, isError, data: contactPoint, error, refetch} = useGetContactPoint(pageId);
+    const { isLoading, isError, data: contactPoint, error, refetch } = useGetContactPoint(pageId);
     const linksInDownLoads = ref<Link[]>();
     watch(() => route.params.id, (newId) => {
       pageId.value = newId;
