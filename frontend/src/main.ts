f8c7f785e5578a4754f81ef831ce6e6f590a973e
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2024-05-13 20:25:06 +0200

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,59 +1,109 @@
-import Vue, { provide, VNode } from "vue";
-import "babel-polyfill";
-import Vuetify from "./core/plugins/vuetify";
-import store from "./core/core.store";
-import App from "./core/App.vue";
-import router from "./core/core.router";
-import moment from "moment";
-import Toast from "vue-toastification";
-import i18n from "@/core/plugins/i18n";
-import { VueQueryPlugin } from "@tanstack/vue-query";
-import vuetify, { adminTheme, kobitTheme } from "@/core/plugins/vuetify";
+import Vue from 'vue';
+import Router from 'vue-router';
+import vuetify, { adminTheme, kobitTheme } from '@/core/plugins/vuetify';
 
-Vue.config.productionTip = false;
+import { theMainRoutes } from '@/features/the-main/the-main.routes';
+import { erfahreMehrRoutes } from '@/features/the-additional/the-additional.routes';
+import { conflictPreventionRoutes } from '@/features/the-additional/features/the-conflict-prevention/the-conflict-prevention.routes';
+import { downloadsRoutes } from '@/features/the-additional/features/the-downloads/the-downloads.routes';
+import { faqRoutes } from '@/features/the-additional/features/the-faq/the-faq.routes';
+import { glossarRoutes } from '@/features/the-additional/features/the-glossar/the-glossar.routes';
+import { leadershipCooperationRoutes } from '@/features/the-additional/features/the-leadership-cooperation/the-leadership-cooperation.routes';
+import { dvFairRoutes } from '@/features/the-additional/features/the-dv-fair/the-dv-fair.routes';
+import { escalationStepsRoutes } from '@/features/the-additional/features/the-escalation-steps/the-escalation-steps.routes';
+import { theAnlaufstellenRoutes } from '@/features/the-unterstuetzungsfinder/features/the-contact-points/the-contact-points.routes';
+import { theAngeboteRoutes } from '@/features/the-angebote/the-angebote.routes';
+import { theUnterstuetzungsfinderRoutes } from '@/features/the-unterstuetzungsfinder/the-unterstuetzungsfinder.routes';
+import { theAnlaufstellenDetailsRoutes } from '@/features/the-unterstuetzungsfinder/features/the-contact-points/the-contact-points-details.routes';
 
-Vue.use(Toast, {
-  position: "bottom-left",
-  transition: "Vue-Toastification__fade",
-  closeOnClick: false,
+import { adminRoutes } from '@/features/admin/the-admin-routes';
+import { adminContactPointsRoutes } from '@/features/admin/features/the-contact-points/the-contact-points-routes';
+import { adminExperienceMoreRoutes } from '@/features/admin/features/the-additional/the-additional-overview-routes';
+import { adminConflictPreventionRoutes } from '@/features/admin/features/the-additional/features/the-conflict-prevention/the-conflict-prevention.routes';
+import { adminDownloadsRoutes } from '@/features/admin/features/the-additional/features/the-downloads/the-downloads.routes';
+import { adminFaqRoutes } from '@/features/admin/features/the-additional/features/the-faq/the-faq.routes';
+import { adminGlossarRoutes } from '@/features/admin/features/the-additional/features/the-glossar/the-glossar.routes';
+import { adminLeadershipCooperationRoutes } from '@/features/admin/features/the-additional/features/the-leadership-cooperation/the-leadership-cooperation.routes';
+import { adminUnterstuetzungsfinderRoutes } from '@/features/admin/components/u-finder/u-finder.routes';
+
+import { getAdminUserInfo } from '@/features/admin/components/userinformation/api/AdminInfoClient';
+
+Vue.use(Router);
+
+const baseRoutes = [
+    theMainRoutes,
+    theUnterstuetzungsfinderRoutes,
+    erfahreMehrRoutes,
+    conflictPreventionRoutes,
+    downloadsRoutes,
+    faqRoutes,
+    glossarRoutes,
+    leadershipCooperationRoutes,
+    dvFairRoutes,
+    escalationStepsRoutes,
+    theAnlaufstellenRoutes,
+    theAnlaufstellenDetailsRoutes,
+    theAngeboteRoutes
+];
+
+const router = new Router({
+    base: process.env.BASE_URL,
+    routes: baseRoutes
 });
 
-Vue.use(VueQueryPlugin);
+let adminRoutesAdded = false;
 
-moment.locale(window.navigator.language);
+router.beforeEach(async (to, from, next) => {
+    if (!adminRoutesAdded && to.path.startsWith('/admin')) {
+        try {
+            const adminUserInfo = await getAdminUserInfo();
 
-// Set up the navigation guard before creating the Vue instance
-router.beforeEach((to, _, next) => {
-  if (to.path === "/admin" || to.path.startsWith("/admin/")) {
-    vuetify.framework.theme.themes.light = adminTheme.themes.light;
-    vuetify.framework.theme.themes.dark = adminTheme.themes.dark;
-  } else {
-    vuetify.framework.theme.themes.light = kobitTheme.themes.light;
-    vuetify.framework.theme.themes.dark = kobitTheme.themes.dark;
-  }
-  next();
+            let adminRoutesToAdd: string | any[] = [];
+
+            if (adminUserInfo.isCentralAdmin) {
+                adminRoutesToAdd = [
+                    ...adminRoutesToAdd,
+                    adminRoutes,
+                    adminContactPointsRoutes,
+                    adminExperienceMoreRoutes,
+                    adminConflictPreventionRoutes,
+                    adminDownloadsRoutes,
+                    adminFaqRoutes,
+                    adminGlossarRoutes,
+                    adminLeadershipCooperationRoutes,
+                    adminUnterstuetzungsfinderRoutes
+                ];
+            } else if (adminUserInfo.isDepartmentAdmin) {
+                adminRoutesToAdd = [
+                    ...adminRoutesToAdd,
+                    adminRoutes,
+                    adminContactPointsRoutes
+                ];
+            }
+
+            if (adminRoutesToAdd.length > 0) {
+                router.addRoutes(adminRoutesToAdd);
+                adminRoutesAdded = true;
+
+                vuetify.framework.theme.themes.light = adminTheme.themes.light;
+                vuetify.framework.theme.themes.dark = adminTheme.themes.dark;
+
+                next({ path: to.path, query: to.query, replace: true });
+            } else {
+                vuetify.framework.theme.themes.light = adminTheme.themes.light;
+                next({ path: '/' });
+            }
+            vuetify.framework.theme.themes.light = adminTheme.themes.light;
+            vuetify.framework.theme.themes.dark = adminTheme.themes.dark;
+        } catch (error) {
+            vuetify.framework.theme.themes.light = adminTheme.themes.light;
+            next({ path: '/' });
+        }
+    } else {
+        vuetify.framework.theme.themes.light = kobitTheme.themes.light;
+        vuetify.framework.theme.themes.dark = kobitTheme.themes.dark;
+        next();
+    }
 });
 
-new Vue({
-  i18n,
-  router,
-  store: store,
-  vuetify: Vuetify,
-  render: (h): VNode => h(App),
-  setup() {
-    provide("store", store);
-  },
-}).$mount("#app");
-
-// Use Vue.nextTick to wait for the theme to be applied before rendering the page
-Vue.nextTick(() => {
-  new Vue({
-    router,
-    store,
-    vuetify: Vuetify,
-    render: (h): VNode => h(App),
-    setup() {
-      provide("store", store);
-    },
-  }).$mount("#app");
-});
+export default router;
