c7aa516499cbeee17e17d06d7687b8841ba47f14
jannik.lange <jannik.lange@muenchen.de>
2024-06-06 17:10:16 +0200

    :bento: changed background picture and color #46

--- frontend/src/features/the-background/the-background.vue
+++ frontend/src/features/the-background/the-background.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="@/assets/background-ufo.svg" style="position: absolute; right: 0;bottom: 0;"/>
+  <img src="@/assets/background-ufo-transparent.svg" style="position: absolute; right: 0;bottom: 0; width: 100%"/>
 </template>
 
 <script lang="ts">
