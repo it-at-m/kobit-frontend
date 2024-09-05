777be6e79dc3505982da0f853882f7076be10018
jannik.lange <jannik.lange@muenchen.de>
2024-09-05 13:07:37 +0200

    spelling error #46

--- frontend/src/features/the-unterstuetzungsfinder/the-unterstuetzungsfinder.routes.ts
+++ frontend/src/features/the-unterstuetzungsfinder/the-unterstuetzungsfinder.routes.ts
@@ -4,7 +4,7 @@ export const THE_UNTERSTUETZUNGSFINDER_ROUTE_PATH = "/unterstuetzungsfinder";
 export const THE_UNTERSTUETZUNGSFINDER_ROUTE_NAME = "Unterstützungsfinder";
 export const THE_UNTERSTUETZUNGSFINDER_ROUTE_META_ICON = "mdi-arrow-decision";
 export const THE_UNTERSTUETZUNGSFINDER_ROUTE_META_INFO_TEXT = `Auf dieser Seite erhalten Sie eine Empfehlung für die geeignete Anlaufstelle in Ihrer aktuellen Situation. Wenige Fragen helfen Ihnen bei der Auswahl. `;
-export const THE_UNTERSTUETZUNGSFINDER_ROUTE_META_INFO_TEXT_COMPLETE = `Auf dieser Seite erhalten Sie eine Empfehlung für die geeignete Anlaufstelle in Ihrer aktuellen Situation. Wenige Fragen helfen Ihnen bei der Auswahl. Klicken Sie dazu jeweils das Feld an, dass aus ihrer Sicht am besten zum Anliegen passt. Es werden keinerlei Informationen gespeichert. `;
+export const THE_UNTERSTUETZUNGSFINDER_ROUTE_META_INFO_TEXT_COMPLETE = `Auf dieser Seite erhalten Sie eine Empfehlung für die geeignete Anlaufstelle in Ihrer aktuellen Situation. Wenige Fragen helfen Ihnen bei der Auswahl. Klicken Sie dazu jeweils das Feld an, dass aus Ihrer Sicht am besten zum Anliegen passt. Es werden keinerlei Informationen gespeichert. `;
 
 export const theUnterstuetzungsfinderRoutes = {
     name: THE_UNTERSTUETZUNGSFINDER_ROUTE_NAME,
