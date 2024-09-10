b21dba66e0f87e937ab1c3d0ffff24873dcc9f78
langehm <38280183+langehm@users.noreply.github.com>
2024-09-10 10:02:04 +0200

    Update frontend/src/features/the-additional/the-additional.routes.ts
    reverted comment

--- frontend/src/features/the-additional/the-additional.routes.ts
+++ frontend/src/features/the-additional/the-additional.routes.ts
@@ -2,7 +2,7 @@ import TheErfahreMehr from "@/features/the-additional/the-additional.vue";
 export const THE_ERFAHRE_MEHR_ROUTE_PATH = "/erfahre-mehr";
 export const THE_ERFAHRE_MEHR_ROUTE_NAME = "Erfahre Mehr";
 export const THE_ERFAHRE_MEHR_ROUTE_META_ICON = "mdi-school";
-export const THE_ERFAHRE_MEHR_ROUTE_META_INFO_TEXT = "Hier finden Sie Informationen zu allen Anlaufstellen.";
+export const THE_ERFAHRE_MEHR_ROUTE_META_INFO_TEXT = "Hier finden Sie Informationen zum Thema „Umgang mit Konflikten“, von der DV-fair über die Eskalationsstufen nach Glasl bis hin zu FAQs.";
 
 
 export const erfahreMehrRoutes = {
