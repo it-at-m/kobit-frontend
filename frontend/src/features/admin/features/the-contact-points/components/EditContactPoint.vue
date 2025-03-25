fd2af5d67a737ea5a295a7c5a8e997fb122a028f
jannik.lange <jannik.lange@muenchen.de>
2025-03-25 09:11:23 +0100

    changed referatsItemName from PLAN to PLA

--- frontend/src/features/admin/features/the-contact-points/components/EditContactPoint.vue
+++ frontend/src/features/admin/features/the-contact-points/components/EditContactPoint.vue
@@ -436,7 +436,7 @@ export default defineComponent({
   data: () => ({
     isFormValid: false,
     referatItems: [
-        "ITM", "RIT", "POR", "BAU", "GSR", "KR", "KVR", "KUL", "MOR", "POR", "RAW", "RBS", "RKU", "PLAN", "SOZ", "SKA"
+        "ITM", "RIT", "POR", "BAU", "GSR", "KR", "KVR", "KUL", "MOR", "POR", "RAW", "RBS", "RKU", "PLA", "SOZ", "SKA"
     ]
   }),
   setup(props) {
